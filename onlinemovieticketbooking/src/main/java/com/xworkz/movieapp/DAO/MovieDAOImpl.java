a600cf72954007e8e9d2207fd1e4246bd2826333
yashu12374 <yashukallesh46@gmail.com>
2025-12-05 08:59:22 +0530

    Added sql table inside package

--- onlinemovieticketbooking/src/main/java/com/xworkz/movieapp/DAO/MovieDAOImpl.java
+++ onlinemovieticketbooking/src/main/java/com/xworkz/movieapp/DAO/MovieDAOImpl.java
@@ -2,6 +2,7 @@ package com.xworkz.movieapp.DAO;
 
 import com.xworkz.movieapp.DTO.MovieDTO;
 import com.xworkz.movieapp.constants.DBConstants;
+import lombok.var;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -13,6 +14,8 @@ public class MovieDAOImpl implements MovieDAO{
     @Override
     public void save(MovieDTO movieDTO) {
 
+        String checkQuery = "SELECT COUNT(*) FROM movie_ticket WHERE theatre_name = ?";
+
         String insertQuery = "Insert Into movie_ticket(ticket_id, theatre_name, movie_name, showTime, number_of_ticket, price)VALUES(?,?,?,?,?,?)";
 
 
@@ -20,6 +23,19 @@ public class MovieDAOImpl implements MovieDAO{
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection(DBConstants.URL.getPro(),DBConstants.USERNAME.getPro(),DBConstants.SECRET.getPro());
 
+            PreparedStatement checkStmt = connection.prepareStatement(checkQuery);
+            checkStmt.setString(1, movieDTO.getTheatre_name());
+
+            var rs = checkStmt.executeQuery();
+
+            int count = 0;
+            if (rs.next()) {
+                count = rs.getInt(1);
+            }
+            if (count > 0) {
+                System.out.println("Duplicate theatre_name found! Record NOT inserted.");
+                return;
+            }
             PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
             preparedStatement.setInt(1,movieDTO.getTicket_id());
             preparedStatement.setString(2,movieDTO.getTheatre_name());
